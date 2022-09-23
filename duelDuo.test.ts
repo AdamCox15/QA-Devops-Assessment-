15e4fe5e9e93b4b0784ab58a76b9a7e190f9064e
Adam Cox <adamcox.dev@gmail.com>
2022-09-23 17:26:27 -0600

    Update duelDuo.test.ts

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,10 +1,9 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const {bots, playerRecord} = require('./data')
-const {shuffleArray} = require('./utils')
 
-app.use(express.json())
+import { Builder, Capabilities, By } from "selenium-webdriver"
+
+require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
   accessToken: '966bedaa93da4cad95809db0786db959',
@@ -15,87 +14,36 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
-// app.use(express.static('/public'))
 
-// app.get('/', (req,res) => {
-//     res.send('It is working')
-
-
-    app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '/public/index.html'))
-    })
-    app.get('/js', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public/index.js'))
-    })
-    app.get('/styles', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public/index.css'))
-    })
-
-app.get('/api/robots', (req, res) => {
-    try {
-        rollbar.
-        res.status(200).send(botsArr)
-    } catch (error) {
-        rollbar.log('Bots did not load')
-        console.log('ERROR GETTING BOTS', error)
-        res.sendStatus(400)
-    }
+beforeEach(async () => {
+    driver.get('http://localhost:3000/')
 })
 
-app.get('/api/robots/five', (req, res) => {
-    try {
-        let shuffled = shuffleArray(bots)
-        let choices = shuffled.slice(0, 5)
-        let compDuo = shuffled.slice(6, 8)
-        res.status(200).send({choices, compDuo})
-    } catch (error) {
-        console.log('ERROR GETTING FIVE BOTS', error)
-        res.sendStatus(400)
-    }
+afterAll(async () => {
+    driver.quit()
 })
 
-app.post('/api/duel', (req, res) => {
-    try {
-        // getting the duos from the front end
-        let {compDuo, playerDuo} = req.body
-
-        // adding up the computer player's total health and attack damage
-        let compHealth = compDuo[0].health + compDuo[1].health
-        let compAttack = compDuo[0].attacks[0].damage + compDuo[0].attacks[1].damage + compDuo[1].attacks[0].damage + compDuo[1].attacks[1].damage
-        
-        // adding up the player's total health and attack damage
-        let playerHealth = playerDuo[0].health + playerDuo[1].health
-        let playerAttack = playerDuo[0].attacks[0].damage + playerDuo[0].attacks[1].damage + playerDuo[1].attacks[0].damage + playerDuo[1].attacks[1].damage
-        
-        // calculating how much health is left after the attacks on each other
-        let compHealthAfterAttack = compHealth - playerAttack
-        let playerHealthAfterAttack = playerHealth - compAttack
-
-        // comparing the total health to determine a winner
-        if (compHealthAfterAttack > playerHealthAfterAttack) {
-            playerRecord.losses++
-            res.status(200).send('You lost!')
-        } else {
-            playerRecord.losses++
-            res.status(200).send('You won!')
-        }
-    } catch (error) {
-        console.log('ERROR DUELING', error)
-        res.sendStatus(400)
-    }
+test('Title shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
 })
 
-app.get('/api/player', (req, res) => {
-    try {
-        res.status(200).send(playerRecord)
-    } catch (error) {
-        console.log('ERROR GETTING PLAYER STATS', error)
-        res.sendStatus(400)
-    }
+test('Choices display when draw button is clicked', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(300)
+    const choicesDiv = await driver.findElement(By.id('choices'))
+
+    expect(choicesDiv).not.toBe('')
 })
 
-const port = process.env.PORT || 3000
+test('Check to see if selecting a bot displays it in our new div', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(300)
+    await driver.findElement(By.xpath('(//*[text()="Add to Duo"])[1]')).click()
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+    const playerDuoDiv = await driver.findElement(By.id('player-duo'))
+    const displayed = await playerDuoDiv.isDisplayed()
+
+    expect(displayed).toBe(true)
 })

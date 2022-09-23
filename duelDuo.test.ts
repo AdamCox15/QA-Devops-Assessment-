de144fb732d510e438c343702e48f100c768e677
Adam Cox <adamcox.dev@gmail.com>
2022-09-23 17:19:11 -0600

    Update duelDuo.test.ts

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -1,9 +1,9 @@
 
 import { Builder, Capabilities, By } from "selenium-webdriver"
 
-require('chromedriver')
+ require('chromedriver')
 
-const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
 
 beforeEach(async () => {
     driver.get('http://localhost:3000/')
@@ -16,5 +16,24 @@ afterAll(async () => {
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Choices display when draw button is clicked', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(300)
+    const choicesDiv = await driver.findElement(By.id('choices'))
+
+    expect(choicesDiv).not.toBe('')
+})
+
+test('Check to see if selecting a bot displays it in our new div', async () => {
+    await driver.findElement(By.id('draw')).click()
+    await driver.sleep(300)
+    await driver.findElement(By.xpath('(//*[text()="Add to Duo"])[1]')).click()
+
+    const playerDuoDiv = await driver.findElement(By.id('player-duo'))
+    const displayed = await playerDuoDiv.isDisplayed()
+
     expect(displayed).toBe(true)
 })
